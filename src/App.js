19d19cabf8f6579914717f1468f18e9130961552
Brian-mula <mulatiwangilabrian@gmail.com>
2023-09-13 17:12:08 +0300

    added app js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+const messages = [
+  "Learn React",
+  "Look for employement",
+  "Use your income to buy a house",
+]
+
 
 function App() {
+  const [step, setStep] = useState(1);
+  const nextStep = () => {
+    if (step > messages.length - 1) {
+      return;
+    }
+    setStep(step + 1);
+  }
+  const prevStep = () => {
+    if (step === 1) {
+      return;
+    }
+    setStep(step - 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center ">
+      <div className="py-10 px-4 bg-gray-100 w-96 shadow-md rounded mt-20">
+        <div className="grid grid-cols-3 gap-4">
+          <div className={`rounded-full p-2 w-10 h-10 flex justify-center items-center ${step >= 1 ? 'bg-purple-700': 'bg-white'}`}>
+            1
+          </div>
+          <div className={`rounded-full p-2 w-10 h-10 flex justify-center items-center ${step >= 2 ? 'bg-purple-700': 'bg-white'}`}>
+            2
+          </div>
+          <div className={`rounded-full p-2 w-10 h-10 flex justify-center items-center ${step >= 3 ? 'bg-purple-700': 'bg-white'}`}>
+            3
+          </div>
+        </div>
+        <div className="my-10 p-3">
+    <p>Step: {step} - {messages[step - 1]}</p>
+        </div>
+        <div className="flex justify-between items-center">
+          <button onClick={prevStep} className="btn btn-primary" >Previous</button>
+          <button onClick={nextStep} className="btn btn-primary">Next</button>
+        </div>
+      </div>
     </div>
   );
 }
